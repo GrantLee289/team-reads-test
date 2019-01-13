eb7c85b268e20ee7adad9be262cc201f2295275b
Grant Lee <grantlee289@gmail.com>
2019-01-13 20:08:50 +0000

    Basic GUI to run scripts added.

--- src/Selenium/RunScripts.java
+++ src/Selenium/RunScripts.java
@@ -1,13 +1,57 @@
 package Selenium;
 
 import java.util.concurrent.TimeUnit;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class RunScripts extends TryCatchTest{
+public class RunScripts extends TryCatchTest implements ActionListener {
+	JButton button;
+	JFrame frame;
+	JLabel label;
 	
 	public static void main (String[] args) {
 
+		RunScripts gui = new RunScripts(); 
+		gui.go();
+	}
+	
+	public void go() {
+		int red = 80;
+		int green = 95;
+		int blue = 130;
+		
+		JFrame frame = new JFrame(); 
+		button = new JButton("Run Test");
+		button.addActionListener(this);
+		
+		graphicsWidget image = new graphicsWidget();
+		
+		label = new JLabel("<html><font color = 'white'>Team<font color='#228B22'>Reads</font></html>");
+		label.setFont(new Font("Open Sans", Font.BOLD, 20));
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.add(label);
+        panel.setBackground(new Color(red,green,blue));
+
+		//frame.getContentPane().add(BorderLayout.NORTH, label);
+        frame.getContentPane().add(BorderLayout.NORTH, panel);
+		frame.getContentPane().add(BorderLayout.CENTER, image);
+		frame.getContentPane().add(BorderLayout.SOUTH, button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,200);
+		frame.setVisible(true);
+		frame.getContentPane().setBackground(new Color(red,green,blue));
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		run();
+	}
+	
+	public void run() {
 		getBaseUrlFirst();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		TestAndRetry();
+		driver.quit();
 	}
+	
 }
---
+++ src/Selenium/graphicsWidget.java
@@ -0,0 +1,18 @@
+package Selenium;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class graphicsWidget extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void paintComponent(Graphics g) {
+		
+		Image image = new ImageIcon("/Users/Grant/eclipse-workspace/team-reads-test/src/Selenium/LogoWhiteFinal.png").getImage();
+		g.drawImage(image,20,4,this);
+	}
+}
